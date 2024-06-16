fa3741e01efe4066e49ebc453d506e7b16904990
rungeAm <rungeandreasm@gmail.com>
2024-06-16 10:28:12 +0200

    ip change

--- P2P_Messenger_(INFO2_PROJ)/Client.cpp
+++ P2P_Messenger_(INFO2_PROJ)/Client.cpp
@@ -43,7 +43,7 @@ int ClientStartup(std::string IP, int PORT)
 	char buffer[1024] = { 0 };
 	string message = "Hello World! ";
 	//strcpy_s(buffer ,message.c_str());			//stringcopy 
-	strncpy_s(buffer, sizeof(buffer), message.c_str(), _TRUNCATE);
+	strncpy_s(buffer, sizeof(buffer)-1, message.c_str(), _TRUNCATE);
 
 	send_(clientSocket, buffer);
 }
